b308b4dcfc4920da4c2a7b3266b55c7233a3dafc
DongHyuk <ciw0504@naver.com>
2021-10-27 17:11:04 +0900

    GameLauncher Update
    1. DB 연동을 통한 로그인 시스템가능.
    2. 엔터키를 눌렀을때 ID와 Password를 통하여 로그인 가능.

--- GameLauncher/GameLauncher/GameLauncher.cpp
+++ GameLauncher/GameLauncher/GameLauncher.cpp
@@ -1,17 +1,57 @@
 #include "GameLauncher.h"
+#include <QTextCodec>
 
 GameLauncher::GameLauncher(QWidget *parent)
     : QMainWindow(parent)
 {
-
+    // DB생성 및 로그인
+    my_LoginDB = new C2DB();
+    my_LoginDB->ConnectDB("192.168.0.56", "CDH", "ehxk2Rnfwoa!", "LOGIN", 3306);
+    codec = QTextCodec::codecForName("EUC-KR");	// 한글코덱
     ui.setupUi(this);
 
     connect(ui.pushButton, SIGNAL(clicked()), this, SLOT(ButtonClicked()));
+
+    // 엔터키를 통한 로그인가능.
+    Enter_Key = new QShortcut(this);
+    Enter_Key->setKey(Qt::Key_Return);
+    connect(Enter_Key, SIGNAL(activated()), this, SLOT(EnterEvent()));
 }
 
 void GameLauncher::ButtonClicked()
 {
-    this->hide();
-    GamePlayLauncher playLauncher;
-    playLauncher.exec();
+    /// 입력받은 아이디와 패스워드 가져오기.
+    QString _ID = ui.lineEdit->text();
+    QString _Password = ui.lineEdit_2->text();
+	std::string _ID_String = _ID.toLocal8Bit().constData();
+    std::string _Password_String = _Password.toLocal8Bit().constData();
+
+    if (my_LoginDB->SearchID(_ID_String))
+    {
+        if (my_LoginDB->ComparePassword(_ID_String, _Password_String))
+        {
+            this->hide();
+			GamePlayLauncher playLauncher;
+			playLauncher.exec();
+            return;
+        }
+        else
+        {
+            ui.label_4->setText(ConvertKR("잘못된 패스워드입니다."));
+        }
+    }
+    else
+    {
+		ui.label_4->setText(ConvertKR("등록되지 않은 아이디입니다."));
+    }
+}
+
+void GameLauncher::EnterEvent()
+{
+    ButtonClicked();
+}
+
+QString GameLauncher::ConvertKR(QByteArray _Text)
+{
+    return codec->toUnicode(_Text);
 }
--- GameLauncher/GameLauncher/GameLauncher.h
+++ GameLauncher/GameLauncher/GameLauncher.h
@@ -2,8 +2,10 @@
 
 #include <QtWidgets/QMainWindow>
 #include <QMessageBox>
+#include <QShortcut>
 #include "ui_GameLauncher.h"
 #include "GamePlayLauncher.h"
+#include "C2DB.h"
 
 class GameLauncher : public QMainWindow
 {
@@ -14,7 +16,15 @@ public:
 
 private:
     Ui::GameLauncherClass ui;
+    // 로그인정보를 조회하기위한 DB
+    C2DB* my_LoginDB = nullptr;
+    QTextCodec* codec;	// 한글 사용을 위한 코덱
+    QShortcut* Enter_Key; // 엔터키를 통한 로그인
 
 public slots:
     void ButtonClicked();
+    void EnterEvent();
+
+private:
+    QString ConvertKR(QByteArray _Text);
 };
